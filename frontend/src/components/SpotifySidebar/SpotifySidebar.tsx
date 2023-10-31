dfc789a2cd9ed4c491c7d19e3ce15a8714443780
Tyler Gilman <tylersgilman@gmail.com>
2023-10-31 17:17:50 -0400

    Created sidebar for displaying Spotify 'jukebox'

---
+++ frontend/src/components/SpotifySidebar/SpotifySidebar.tsx
@@ -0,0 +1,46 @@
+import { Heading, ListItem, OrderedList, StackDivider, VStack, Button, Input} from '@chakra-ui/react';
+import React from 'react';
+
+const sorted = [{id: '1', name: 'song1'}, {id: '2', name: 'song2'}, {id: '3', name: 'song3'}];
+
+export default function SpotifySidebar(): JSX.Element {
+  return (
+    <VStack
+      align='left'
+      spacing={2}
+      border='2px'
+      padding={2}
+      marginLeft={2}
+      borderColor='gray.500'
+      height='100%'
+      divider={<StackDivider borderColor='gray.200' />}
+      borderRadius='4px'>
+      <Heading fontSize='xl' as='h1'>
+        Spotify Controls
+      </Heading>
+      <div>
+        Current Song: {sorted[0].name} <Button size='xs'>Vote Skip</Button>
+      </div>
+      <div>
+        Upcoming Songs:
+      </div>
+      <OrderedList>
+        {sorted.slice(1).map(song => (
+          <ListItem key={song.id}>
+            <div>
+              {song.name} <Button size='xs'>Upvote</Button><Button size='xs'>Downvote</Button>
+            </div>
+          </ListItem>
+        ))}
+      </OrderedList>
+      <div>
+        Add Song
+      </div>
+      <div>
+        Search: <Input size='sm' />
+      </div>
+      <div> Search Results </div>
+      <div> </div>
+    </VStack>
+  );
+}
