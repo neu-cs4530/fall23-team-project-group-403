ed6fb5d8f6bdf6bf86b018da33e7c3493b2b96c7
jritchie02 <jaredr_22@yahoo.com>
2023-11-26 10:14:08 -0500

    Disable controller on search input, debounce search call

--- frontend/src/components/SpotifySidebar/Controls/SettingsDisplay.tsx
+++ frontend/src/components/SpotifySidebar/Controls/SettingsDisplay.tsx
@@ -1,19 +1,59 @@
 import { Box, Text, Heading, Input, VStack, StackDivider } from '@chakra-ui/react';
-import { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Song } from '../../../hooks/useQueue';
 import { useSpotify } from '../../../hooks/useSpotify';
+import useTownController from '../../../hooks/useTownController';
 import VolumeSlider from './VolumeSlider';
 
 const SettingsDisplay = () => {
   const { searchForTrack, changeSpotifyVolume } = useSpotify();
+  const coveyTownController = useTownController();
   const [songResults, setSongResults] = useState<Song[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   console.log(songResults);
 
-  const handleKeyInput = async (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const searchResults = await searchForTrack(event.currentTarget.value);
-    setSongResults(searchResults);
+  // Create a debounce function so we only send a new search API call after a time interval
+  // This prevents us from sending a new API call for every key stroke
+  const debounce = (func: () => void, delay: number) => {
+    let timeoutId: ReturnType<typeof setTimeout> | null;
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      timeoutId = setTimeout(func, delay);
+    };
   };
+
+  // Debounce search call, waits 300 ms before calling search API
+  const debouncedSearch = debounce(async () => {
+    const searchResults = await searchForTrack(inputRef.current?.value || '');
+    setSongResults(searchResults);
+  }, 300);
+
+  const handleKeyInput = () => {
+    debouncedSearch();
+  };
+
+  useEffect(() => {
+    // Pause the town when the input is focused, we don't want to move while searching
+    const handleFocusChange = () => {
+      if (document.activeElement !== inputRef.current) {
+        coveyTownController.unPause();
+      } else {
+        coveyTownController.pause();
+      }
+    };
+
+    inputRef.current?.addEventListener('focus', handleFocusChange);
+    inputRef.current?.addEventListener('blur', handleFocusChange);
+
+    return () => {
+      inputRef.current?.removeEventListener('focus', handleFocusChange);
+      inputRef.current?.removeEventListener('blur', handleFocusChange);
+    };
+  }, [inputRef, coveyTownController]);
 
   return (
     <Box>
@@ -29,7 +69,7 @@ const SettingsDisplay = () => {
         borderRadius='4px'>
         <Box>
           <Text>
-            Search <Input onKeyUp={handleKeyInput} size='sm' />
+            Search <Input ref={inputRef} onKeyUp={handleKeyInput} size='sm' />
           </Text>
           <Text> Search Results </Text>
         </Box>
