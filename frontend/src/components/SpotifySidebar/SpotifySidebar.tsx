b3a008c926a7b8d2d37ea12fb0fd1cda25df6b4c
jritchie02 <jaredr_22@yahoo.com>
2023-11-27 20:49:41 -0500

    Disabling spotify when users are not authenticated

--- frontend/src/components/SpotifySidebar/SpotifySidebar.tsx
+++ frontend/src/components/SpotifySidebar/SpotifySidebar.tsx
@@ -2,8 +2,15 @@ import { Heading, StackDivider, VStack } from '@chakra-ui/react';
 import React from 'react';
 import QueueDisplay from './Queue/QueueDisplay';
 import SettingsDisplay from './Controls/SettingsDisplay';
+import { isEmpty } from 'lodash';
 
 export default function SpotifySidebar(): JSX.Element {
+  const spotifyToken = sessionStorage.getItem('SPOTIFY_AUTH_TOKEN');
+
+  if (isEmpty(spotifyToken)) {
+    return <></>;
+  }
+
   return (
     <VStack
       align='left'
@@ -15,7 +22,7 @@ export default function SpotifySidebar(): JSX.Element {
       height='100%'
       divider={<StackDivider borderColor='gray.200' />}
       borderRadius='4px'>
-      <Heading fontSize='xl' as='h1' > 
+      <Heading fontSize='xl' as='h1'>
         Spotify Jukebox
       </Heading>
       <QueueDisplay />
